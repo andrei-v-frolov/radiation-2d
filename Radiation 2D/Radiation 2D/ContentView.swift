bd5835da79421b62f77c4a7502b5ce109de82611
Andrei Frolov <frolov@sfu.ca>
2023-12-01 15:27:52 -0800

    Initial project created.

---
+++ Radiation 2D/Radiation 2D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Radiation 2D
+//
+//  Created by Andrei Frolov on 2023-12-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Radiation 2D/Radiation 2D/Radiation_2DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Radiation_2DApp.swift
+//  Radiation 2D
+//
+//  Created by Andrei Frolov on 2023-12-01.
+//
+
+import SwiftUI
+
+@main
+struct Radiation_2DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
