8fe05b527cb78fcdb4febdda641e69b3aeba7d89
Andrei Frolov <frolov@sfu.ca>
2023-12-24 10:07:24 -0800

    Integrator tests for relativistic motion added.

--- Radiation 2D/Radiation 2D Tests/Radiation 2D Tests.swift
+++ Radiation 2D/Radiation 2D Tests/Radiation 2D Tests.swift
@@ -7,29 +7,27 @@
 
 import XCTest
 
-final class Radiation_2DTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+final class RadiationTests: XCTestCase {
+    override func setUpWithError() throws { super.setUp() }
+    override func tearDownWithError() throws { super.tearDown() }
+    
+    // test tolerance
+    let epsilon: Double = 3.0e-15
+    
+    // test particle motion integrator
+    func testParticle() throws {
+        let alpha = 1.0, dt = 1.0/alpha
+        
+        for i in 1...20 {
+            let psi = Double(i)/10.0, p = sinh(psi), x = -psi/alpha
+            var particle = Particle(state: SIMD4<Double>(x,0,p,0))
+            
+            // compare against exact solution
+            for t in stride(from: 0.0, through: 10.0*dt, by: dt) {
+                XCTAssertEqual(particle.p.x, p*exp(-alpha*t), accuracy: epsilon)
+                XCTAssertEqual(particle.x.x, -asinh(p*exp(-alpha*t))/alpha, accuracy: epsilon)
+                particle.advance(dt, steps: 10)
+            }
         }
     }
-
 }
