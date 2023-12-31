e8ce0d0c6e862d07025fa33ee80fc9f6ed5f36d4
Andrei Frolov <frolov@sfu.ca>
2023-12-30 22:12:59 -0800

    Drag gesture handler added.

--- Radiation 2D/Radiation 2D/SimulationView.swift
+++ Radiation 2D/Radiation 2D/SimulationView.swift
@@ -11,6 +11,16 @@ struct SimulationView: View {
     @State private var trajectory: Trajectory = .racetrack(3.0)
     var time: Double = 0.0
     
+    // move particle around with drag gesture
+    @State private var dragging = false
+    @State private var location = CGPoint.zero
+    
+    var drag: some Gesture {
+        DragGesture(minimumDistance: 0.0)
+            .onChanged { self.dragging = true; self.location = $0.location }
+            .onEnded { _ in self.dragging = false }
+    }
+    
     var body: some View {
         Canvas(rendersAsynchronously: false) { context, size in
             let scale = min(size.width, size.height)/10.0, thickness = 4.0/scale
@@ -24,8 +34,17 @@ struct SimulationView: View {
             let v = trajectory.state(at: time)
             context.stroke(Path { $0.move(to: CGPoint(v.q)); $0.addLine(to: CGPoint(v.q+v.p))}, with: .color(.orange), lineWidth: thickness)
             context.fill(Symbol.circle.shape(at: v.xy, size: 5.0*thickness), with: .color(.red))
+            
+            if (dragging) {
+                let gradient = Gradient(colors: [.red.opacity(0.3), .clear])
+                let d = 25.0*thickness, location = location.applying(context.transform.inverted())
+                let cursor = Path(ellipseIn: CGRect(x: location.x-d/2.0, y: location.y-d/2.0, width: d, height: d))
+                context.fill(cursor, with: .radialGradient(gradient, center: location, startRadius: 0.0, endRadius: d/2.0))
+            }
+
         }
         .background(.gray)
+        .gesture(drag)
     }
 }
 
